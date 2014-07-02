3c2f9e30274ab48b6050cdeb4087d181262c3c47
Vinit Kataria <vinitkataria911@gmail.com>
2014-07-02 13:23:23 +0530

    raaz-curves started

---
+++ raaz-curves/Raaz/Curves.hs
@@ -0,0 +1,12 @@
+{-|
+
+This module exposes all the cryptographic hash functions available
+under the raaz library.
+
+-}
+
+module Curves.Hash
+       ( module Raaz.Hash.Blake256
+       ) where
+
+{-# ANN module "HLint: ignore Use import/export shortcut" #-}
---
+++ raaz-curves/Raaz/Curves/Curve25519/Internal.hs
@@ -0,0 +1,6 @@
+module Raaz.Curves.Curve25519.Internal
+       ( Curve25519(..)
+       ) where
+
+import Raaz.Curves.Curve25519.Type
+import Raaz.Curves.Curve25519.Instance()
