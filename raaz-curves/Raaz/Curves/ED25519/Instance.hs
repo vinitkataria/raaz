de43b09bce1659f8b3c7cb0aec755c6d2e6b944b
Vinit Kataria <vinitkataria911@gmail.com>
2015-05-22 11:52:50 +0530

    Added ED25519 Sign like a hash gadget - Needs to be replaced by Sign gadget like RSA

--- raaz-curves/Raaz/Curves/ED25519/Instance.hs
+++ raaz-curves/Raaz/Curves/ED25519/Instance.hs
@@ -22,7 +22,7 @@ import Raaz.Core.Util.Ptr
 import Raaz.Curves.ED25519.Type
 import Raaz.Curves.ED25519.Ref
 import Raaz.Curves.ED25519.CPortable ()
-
+import Raaz.Curves.ED25519.Internal
 
 ----------------------------- Sign -------------------------------------------
 
@@ -30,18 +30,18 @@ instance CryptoPrimitive Sign where
   type Recommended Sign = CGadget Sign
   type Reference Sign   = HGadget Sign
 
-instance Hash Sign where
-  defaultCxt _ = Sign $ VU.fromList [ 0x6a09e667f3bcc908
-                                      , 0xbb67ae8584caa73b
-                                      , 0x3c6ef372fe94f82b
-                                      , 0xa54ff53a5f1d36f1
-                                      , 0x510e527fade682d1
-                                      , 0x9b05688c2b3e6c1f
-                                      , 0x1f83d9abfb41bd6b
-                                      , 0x5be0cd19137e2179
-                                      ]
+-- instance Hash Sign where
+--   defaultCxt _ = Sign $ VU.fromList [ 0x6a09e667f3bcc908
+--                                       , 0xbb67ae8584caa73b
+--                                       , 0x3c6ef372fe94f82b
+--                                       , 0xa54ff53a5f1d36f1
+--                                       , 0x510e527fade682d1
+--                                       , 0x9b05688c2b3e6c1f
+--                                       , 0x1f83d9abfb41bd6b
+--                                       , 0x5be0cd19137e2179
+--                                       ]
 
-  hashDigest = id
+--   hashDigest = id
 
 instance Gadget (HGadget Sign) where
   type PrimitiveOf (HGadget Sign)  = Sign
