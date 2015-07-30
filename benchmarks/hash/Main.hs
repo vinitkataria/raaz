3a7148182f194a7fba9821389aae21fc3a7cce07
Vinit Kataria <vinitkataria911@gmail.com>
2015-07-31 01:42:03 +0530

    Fixed benchmarks to use newer Criterion package

--- benchmarks/hash/Main.hs
+++ benchmarks/hash/Main.hs
@@ -1,8 +1,8 @@
 import           Data.Version
-import           Criterion.Main    (defaultMainWith)
-import           Criterion         (bgroup)
-import           Criterion.Config  (Config(..), ljust, defaultConfig)
-import           Paths_src         (version)
+import           Criterion.Main          (defaultMainWith)
+import           Criterion               (bgroup)
+import           Criterion.Main.Options
+import           Paths_src               (version)
 
 import qualified Modules.Sha       as Sha
 import qualified Modules.Blake     as Blake
@@ -11,16 +11,11 @@ import           Modules.Defaults
 
 pkgName = "raaz-hash-" ++ showVersion version
 
-myConfig :: Config
-myConfig = defaultConfig {
-  cfgSamples = ljust 10
-  }
-
 main :: IO ()
 main = do putStrLn $ "Running benchmarks for " ++ pkgName
           putStrLn $ "Data Size : " ++ show nSize
           b <- benchmarks
-          defaultMainWith myConfig (return ()) b
+          defaultMainWith defaultConfig b
 
 benchmarks = do
   sha <- Sha.benchmarks
--- benchmarks/modular/Main.hs
+++ benchmarks/modular/Main.hs
@@ -1,9 +1,9 @@
 import           Data.Bits
 import           Data.Version
-import           Criterion.Main    (defaultMainWith)
-import           Criterion         (bgroup)
-import           Criterion.Config  (Config(..), ljust, defaultConfig)
-import           Paths_src         (version)
+import           Criterion.Main          (defaultMainWith)
+import           Criterion               (bgroup)
+import           Criterion.Main.Options
+import           Paths_src               (version)
 import           System.Random
 
 import qualified Modules.Number.Modular as Modular
@@ -20,6 +20,6 @@ main = do
       klist'  = take 100 $ randomRs (1 `shiftL` 500, 1 `shiftL` 501 - 1) gen
       mlist'  = take 100 $ randomRs (1 `shiftL` 500, 1 `shiftL` 501 - 1) gen
   putStrLn $ "Running benchmarks for " ++ pkgName
-  defaultMainWith defaultConfig (return ()) $ benchmarks (Modular.ParamList glist klist mlist glist' klist' mlist')
+  defaultMainWith defaultConfig $ benchmarks (Modular.ParamList glist klist mlist glist' klist' mlist')
 
 benchmarks p = [ bgroup "Modular Exponentiation" $ Modular.benchExponentiation p ]
